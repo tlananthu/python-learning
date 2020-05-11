5411cc5480e2f5f99211ade6334c5613af827719
Anantha Narayanan <tlananthu@gmail.com>
2020-05-12 00:25:00 +0530

    Printing Files List
    Program to print file names recursively

---
+++ 00_simple_examples/02_list_files_recursively.py
@@ -0,0 +1,12 @@
+import sys
+
+def printFiles(rootDir):
+    import glob
+    import os
+
+    files={}
+    for file in glob.iglob(rootDir+'**/*',recursive=True):
+        if os.path.isfile(file):
+            print(str(file))
+
+#printFiles('.')
