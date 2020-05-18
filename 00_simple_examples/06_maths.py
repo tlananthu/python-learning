f3861cbab9d384def8dbff0c10938b18935e98db
Anantha Narayanan <tlananthu@gmail.com>
2020-05-18 14:11:47 +0530

    Create 06_maths.py
    Basic Arithmetic switch based on user input

---
+++ 00_simple_examples/06_maths.py
@@ -0,0 +1,13 @@
+number1=int(input('Please input a number'))
+number2=int(input('Please input another number'))
+
+operation=input('Please input an operation. +-/*')
+
+if operation=='+':
+    print(number1+number2)
+elif operation=='-':
+    print(number2-number2)
+elif operation=='/':
+    print(number2/number2)
+elif operation=='*':
+    print(number2*number2)
