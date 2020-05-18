b3d3baa03712aba9b5fd85aa6d7ecbae91218cf1
Anantha Narayanan <tlananthu@gmail.com>
2020-05-18 13:00:27 +0530

    Update 02_download_jpgs_merge.py
    Added comments

--- 01_intermediate_examples/02_download_jpgs_merge.py
+++ 01_intermediate_examples/02_download_jpgs_merge.py
@@ -1,6 +1,10 @@
 #This program downloads jpgs from the website and creates a pdf out of it
+#Images are sequentially ordered in the website from 1 to 152 for a book "My Varnasrama Dharma by Mahatma Gandhi"
 #https://www.gandhiheritageportal.org/datalink/files/allbooks/en/archive/book675/book_675_en_001.jpg to 152.jpg
 
+#procedure do download one file
+#accepts 2 parameters, fullurl of the jpg file
+#and download filename (including folder)
 def download(fullurl, downloadname):
     import os
     import requests
@@ -19,6 +23,7 @@ def download(fullurl, downloadname):
         with open(fn,'wb') as f:
             f.write(r.content)
 
+#hard-coded logic of 152 files and url from where the jpgs are to be downloaded
 def downloadallJPGS():
     for i in range(152):
         page=str(i+1)
@@ -26,6 +31,7 @@ def downloadallJPGS():
         ofn='book_675_en_{0}.jpg'.format(page.zfill(3))
         download("https://www.gandhiheritageportal.org/datalink/files/allbooks/en/archive/book675/"+ifn,"temp/download_jpgs/"+ofn)
 
+#once jpgs are downloaded it accepts foldername. It appends all files and creates a pdf
 def createPDF(foldername):
     from PIL import Image
     import os
@@ -45,5 +51,7 @@ def createPDF(foldername):
 
     i2.save(foldername+'/book_675_en.pdf',save_all=True, append_images=imageList)
 
+#download happens first
 downloadallJPGS()
+#creates pdf next
 createPDF("temp/download_jpgs")
