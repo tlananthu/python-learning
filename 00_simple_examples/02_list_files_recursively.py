69f9adf2a9872014dc1cc675541b3075f88fa35f
Anantha Narayanan <tlananthu@gmail.com>
2020-05-20 19:07:33 +0530

    Update 02_list_files_recursively.py
    Writes the name of files to log file

--- 00_simple_examples/02_list_files_recursively.py
+++ 00_simple_examples/02_list_files_recursively.py
@@ -1,12 +1,18 @@
 import sys
+import codecs
 
-def printFiles(rootDir):
+def printFiles(rootDir,outname):
     import glob
     import os
+    
+    f=codecs.open(outname,'w','utf-8')
 
     files={}
     for file in glob.iglob(rootDir+'**/*',recursive=True):
-        if os.path.isfile(file):
-            print(str(file))
+        if os.path.isdir(file):
+            print('Scanning {0}'.format(file))
+        elif os.path.isfile(file):
+            f.write(str(file)+'\n')
+    f.close()
 
-printFiles('')
+printFiles('c:/temp','temp/list_recursive/filelist.log')
